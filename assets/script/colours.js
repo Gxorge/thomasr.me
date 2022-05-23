ba26a36b3233db079c76d2bf4c87d5173442d8c4
Tom <git@tomr.me>
2022-05-23 08:54:58 +0100

    improve colour setting code + rainbow!!!

--- assets/script/colours.js
+++ assets/script/colours.js
@@ -8,39 +8,30 @@ function setCssAccent(a, c) {
     document.querySelector(':root').style.setProperty(`--accent${a}`, `${c}`);
 }
 
-function setColour(c) {
-    document.querySelector('meta[name="theme-color"]').content = `${c[0]}`;
+function setColour(hue) {
+    localStorage.setItem('last-colour', hue);
+    document.querySelector('meta[name="theme-color"]').content = `${hslToHex(hue, 40, 50)}`;
 
-    setCssAccent('Dark', c[0]);
-    setCssAccent('DarkIsh', c[1]);
-    setCssAccent('', c[2]);
-    setCssAccent('LightIsh', c[3]);
-    setCssAccent('Light', c[4]);
+    setCssAccent('Dark', hslToHex(hue, 40, 10));
+    setCssAccent('DarkIsh', hslToHex(hue, 50, 20));
+    setCssAccent('', hslToHex(hue, 40, 50));
+    setCssAccent('LightIsh', hslToHex(hue, 50, 80));
+    setCssAccent('Light', hslToHex(hue, 35, 95));
 }
 
 function pickColour(hue) {
-    let baseHue = hue ? hue : Math.floor(Math.random() * (359 - 1 + 1) + 1);
+    let randomHue = hue ? hue : Math.floor(Math.random() * (359 - 1 + 1) + 1);
 
     if(
         // ensure new colour is more than 25deg from previous
-        (Math.abs(parseInt(localStorage.getItem('last-colour')) - baseHue) <= 25)
-        ||
+        (Math.abs(parseInt(localStorage.getItem('last-colour')) - randomHue) <= 25)
+        // ||
         // avoid putrid yellows
-        (baseHue > 45 && baseHue < 120)
+        // (baseHue > 45 && baseHue < 120)
     ) {
         return pickColour();    
     } else {        
-        localStorage.setItem('last-colour', baseHue);
-
-        return setColour([
-            // hue, sat %, lum %
-            // todo: perceived luminance correction 
-            hslToHex(baseHue, 40, 10), // dark
-            hslToHex(baseHue, 50, 20), // darkish
-            hslToHex(baseHue, 40, 50), // base
-            hslToHex(baseHue, 50, 80), // lightish
-            hslToHex(baseHue, 35, 95) // light
-        ]);
+        setColour(randomHue);
     }
 }
 
@@ -65,12 +56,23 @@ function enableColours(noreload) {
 // konami code for colour changing!!
 let pressedKeys = '';
 document.addEventListener('keydown', e => {
-  pressedKeys += e.code;
-  if (pressedKeys == "ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightKeyBKeyA") {
-      window.setInterval(pickColour, 1000);
-  }
+    pressedKeys += e.code;
+    if (pressedKeys == "ArrowUpArrowUpArrowDownArrowDownArrowLeftArrowRightArrowLeftArrowRightKeyBKeyA") {
+        rainbowomg(parseInt(localStorage.getItem('last-colour')));
+    }
 });
 
+
+let rainbowCurrentHue = 0;
+function rainbowomg(startAt) {
+    if(startAt) rainbowCurrentHue = startAt;
+    if(rainbowCurrentHue < 0 || rainbowCurrentHue > 360) rainbowCurrentHue = 0;
+    
+    setColour(rainbowCurrentHue);
+    rainbowCurrentHue++;
+
+    window.setTimeout(rainbowomg, 1);
+} 
 // Thank you, icl7126
 // https://stackoverflow.com/a/44134328/6325767
 function hslToHex(h, s, l) {
